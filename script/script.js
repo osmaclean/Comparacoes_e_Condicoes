f1c0005c42dd442fdddcf9cbd790878967a0a5dc
Lucas Maclean <contatolucasmaclean@gmail.com>
2022-11-02 17:27:02 -0300

    Projeto iniciado e primeira tarefa realizada com sucesso!

---
+++ script/script.js
@@ -0,0 +1,12 @@
+
+// Primeira Tarefa
+
+function exercicio1(){
+let valor1 = 10;
+let valor2 = "10";
+let comparacoesDeValores = (valor1 === valor2);
+console.log(comparacoesDeValores);
+}
+exercicio1();
+
+// Segunda Tarefa
